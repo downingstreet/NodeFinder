d82e92cfaa48433a65f62e2c39371f0734a7de3b
Varun Varshney <no10downingstreet@gmail.com>
2014-05-29 23:57:39 +0530

    Initial commit

---
+++ src/com/major/network/Network/OsFingerprint.java
@@ -0,0 +1,10 @@
+package com.major.network.Network;
+
+public class OsFingerprint {
+
+    // private final static String TAG = "OsFingerprint";
+
+    public static String finger(long[] longArray) {
+        return null;
+    }
+}
---
+++ src/com/major/network/Utils/Help.java
@@ -0,0 +1,14 @@
+package com.major.network.Utils;
+
+import com.major.network.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Help extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help);
+    }
+}
